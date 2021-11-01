3a88d9e620cf59d0bde57475f971dcb290f481bd
Nikita Wilms <nikdotde@gmail.com>
2021-11-01 15:48:22 +0100

    create button component

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Welcome from '../components/dashboard/Welcome';
+import Reminder from '../components/dashboard/Reminder';
+import RoomOrDesk from '../components/dashboard/RoomOrDesk';
+import Footer from '../components/Footer';
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      {/* later on, userName gets input from DB */}
+      <Welcome greeting='Welcome' userName='Jan' />
+      {/* later on, isChecked gets input from DB */}
+      <Reminder isChecked={true} />
+      <RoomOrDesk option1='Room' option2='Desk' />
+      <Footer nav1='Dashboard' nav2='Bookings' nav3='On-Site' />
+    </>
   );
 };
 
